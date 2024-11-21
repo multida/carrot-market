f5ce75f0f16132de8cc522c1ff00ef96269b5aaa
multida <wow_da65@naver.com>
2024-11-21 15:35:47 +0900

    empty, group

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,32 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-4">
-        {["yeah", "Me", "You", "Yourself"].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5  p-2.5 pb-5 border-b-2  last:border-b-0 last:pb-0"
-          >
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-3">
+        {["yeah", "Me", "You", "Yourself", ""].map((person, index) => (
+          <div key={index} className="flex items-center gap-5">
             <div className="size-10 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium">{person}</span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
-              <span>{index}</span>
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:bg-gray-300 empty:animate-pulse">
+              {person}
+            </span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full rounded-ful absolute animate-ping" />
             </div>
           </div>
         ))}
+
+        <div className="group">
+          <input
+            type="email"
+            placeholder="Write your email"
+            className="bg-gray-100 w-full"
+          />
+          <span className="group-focus-within:block hidden">
+            Make sure it is a valid email...
+          </span>
+          <button>Submit</button>
+          {/* group이말 그대로 within 내부에 focus를 갖고 있다면~ */}
+        </div>
       </div>
     </main>
   );
