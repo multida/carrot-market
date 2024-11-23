c32b7d73fa18d91cd09ad415e83db25154c590b4
multida <wow_da65@naver.com>
2024-11-23 16:02:16 +0900

    로그인 페이지 버튼 컴포넌트화

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
         <span className="text-9xl">🥕</span>
         <h1 className="text-4xl">당근</h1>
-        <h2 className="font-2xl">당근 마켓에 어서오세요!</h2>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
         <Link href="/create-account" className="primary-btn text-lg py-2.5">
