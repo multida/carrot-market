40bf741658b4f37b052fe0e3c419751368933d9f
multida <wow_da65@naver.com>
2024-11-23 16:17:49 +0900

    form input에 px-2 추가

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -14,7 +14,7 @@ export default function FormInput({
   return (
     <div className="flex flex-col gap-2">
       <input
-        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 px-2"
         type={type}
         placeholder={placeholder}
         required={required}
