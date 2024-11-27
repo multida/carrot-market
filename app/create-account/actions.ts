2f27739ce135a87a1e61490672aac6719ebd4a3d
multida <wow_da65@naver.com>
2024-11-27 19:16:48 +0900

    Zod - Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,5 +1,16 @@
 "use server";
 import { z } from "zod";
+
+const checkUsername = (username: string) => !username.includes("potato");
+
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
 const formSchema = z
   .object({
     username: z
@@ -9,22 +20,14 @@ const formSchema = z
       })
       .min(3, "Way too short!!!")
       .max(10, "That is too looooong!")
-      .refine(
-        (username) => !username.includes("potato"),
-        "No potatoes allowed!"
-      ),
+      .refine(checkUsername, "No potatoes allowed!"),
     email: z.string().email(),
     password: z.string().min(10),
     confirm_password: z.string().min(10),
   })
-  .superRefine(({ password, confirm_password }, ctx) => {
-    if (password !== confirm_password) {
-      ctx.addIssue({
-        code: "custom",
-        message: "Two passwords should be equal",
-        path: ["confirm_password"],
-      });
-    }
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same!",
+    path: ["confirm_password"],
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
