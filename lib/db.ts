f9b61eecc593d3100a86568e3efb0421d61d5e08
multida <wow_da65@naver.com>
2024-11-28 18:58:58 +0900

    테스트 코드 삭제

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
-db.user.create({
-  data: {
-    username: "fdfs",
-    phone: "12312412",
-  },
-});
 export default db;
