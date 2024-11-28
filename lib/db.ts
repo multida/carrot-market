44656a99f2ecd02ac3b9c25ed8c0c25424ceda13
multida <wow_da65@naver.com>
2024-11-28 21:33:43 +0900

    relationships

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
+async function test() {
+  // const token = await db.sMSToken.create({
+  //   data: {
+  //     token: "1212112",
+  //     user: {
+  //       connect: {
+  //         id: 1,
+  //       },
+  //     },
+  //   },
+  // });
+  // console.log(token);
+
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+test();
+
 export default db;
