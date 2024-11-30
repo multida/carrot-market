cb7da6bb1e1ee477f9a74cc92db71adedda05364
multida <wow_da65@naver.com>
2024-11-30 22:59:55 +0900

    profile, middleware

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -63,6 +63,7 @@ export async function logIn(prevState: any, formData: FormData) {
     if (ok) {
       const session = await getSession();
       session.id = user!.id;
+      await session.save();
       redirect("/profile");
     } else {
       return {
